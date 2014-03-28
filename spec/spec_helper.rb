a388f1fc0eba5d33cabfd8fb206060b8f3217b04
Chuck Callebs <cacallebs@gmail.com>
2014-03-27 22:50:34 -0500

    add rspec skeleton.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,1 @@
+require 'splitter'
---
+++ spec/splitter_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe SplitDateTime::Splitter do
+  context '#inclusion' do
+    class TestClass
+      include SplitDateTime::Splitter
+    end
+    let(:subject) { TestClass.new }
+
+    it 'should create time accessors when included' do
+    end
+
+    it 'should create date accessors when included' do
+    end
+
+    it 'should create callbacks when included' do
+    end
+
+    it 'should create callback method when included' do
+
+    end
+  end
+
+  context '#accessors' do
+    it 'should correctly extract a DateTime date' do
+    end
+
+    it 'should correctly extract a DateTime time' do
+    end
+  end
+
+  context '#callbacks' do
+    it 'should correctly concatenate a field Date/Time together' do
+    end
+  end
+end
