5753616e5cb220f874867882ba389d3b8e96ebc9
Chuck Callebs <cacallebs@gmail.com>
2014-03-27 22:36:50 -0500

    logically break out splitting functionality from rails inclusion.

--- lib/split_date_time.rb
+++ lib/split_date_time.rb
@@ -1,49 +1,22 @@
-require 'active_record'
-require 'active_support/all'
-require 'split_date_time/version'
+require 'split_date_time/splitter'
 
 module SplitDateTime
-  extend ActiveSupport::Concern
-
-  included do
-    extend ClassMethods
-  end
-
-  module ClassMethods
-    def split_date_time(options = {})
-      options[:prefix] ||= options[:field]
-      define_split_accessors(options[:field], options[:prefix])
-      define_concatenation_callback(options[:field], options[:prefix])
-    end
-
-    def define_split_accessors(field, prefix)
-      define_method :"#{prefix}_time" do
-        field_val = send(field)
-        field_val.strftime('%H:%M') if field_val.present?
-      end
-      define_method :"#{prefix}_time=" do |val|
-        instance_variable_set :"@#{prefix}_time", val
-      end
-      define_method :"#{prefix}_date" do
-        field_val = send(field)
-        field_val.strftime('%m/%d/%Y') if field_val.present?
-      end
-      define_method :"#{prefix}_date=" do |val|
-        instance_variable_set :"@#{prefix}_date", val
-      end
-    end
-
-    def define_concatenation_callback(field, prefix)
-      before_save :"concatenate_#{field}"
-      define_method :"concatenate_#{field}" do
-        Time do
-          date_val = instance_variable_get :"@#{prefix}_date"
-          time_val = instance_variable_get :"@#{prefix}_time"
-          self.send "#{field}=", Time.parse("#{date_val} #{time_val}")
+  if defined? Rails::Railtie
+    require 'rails'
+    class Railtie < Rails::Railtie
+      initializer 'split_date_time.insert_into_active_record' do
+        ActiveSupport.on_load :active_record do
+          SplitDateTime::Railtie.insert
         end
       end
     end
   end
-end
 
-ActiveRecord::Base.send :include, SplitDateTime
+  class Railtie
+    def self.insert
+      if defined?(ActiveRecord)
+        ActiveRecord::Base.send(:include, SplitDateTime::Splitter)
+      end
+    end
+  end
+end
---
+++ lib/split_date_time/splitter.rb
@@ -0,0 +1,49 @@
+require 'active_record'
+require 'active_support/all'
+require 'split_date_time/version'
+
+module SplitDateTime
+  module Splitter
+    extend ActiveSupport::Concern
+
+    included do
+      extend ClassMethods
+    end
+
+    module ClassMethods
+      def split_date_time(options = {})
+        options[:prefix] ||= options[:field]
+        define_split_accessors(options[:field], options[:prefix])
+        define_concatenation_callback(options[:field], options[:prefix])
+      end
+
+      def define_split_accessors(field, prefix)
+        define_method :"#{prefix}_time" do
+          field_val = send(field)
+          field_val.strftime('%H:%M') if field_val.present?
+        end
+        define_method :"#{prefix}_time=" do |val|
+          instance_variable_set :"@#{prefix}_time", val
+        end
+        define_method :"#{prefix}_date" do
+          field_val = send(field)
+          field_val.strftime('%m/%d/%Y') if field_val.present?
+        end
+        define_method :"#{prefix}_date=" do |val|
+          instance_variable_set :"@#{prefix}_date", val
+        end
+      end
+
+      def define_concatenation_callback(field, prefix)
+        before_save :"concatenate_#{field}"
+        define_method :"concatenate_#{field}" do
+          Time do
+            date_val = instance_variable_get :"@#{prefix}_date"
+            time_val = instance_variable_get :"@#{prefix}_time"
+            self.send "#{field}=", Time.parse("#{date_val} #{time_val}")
+          end
+        end
+      end
+    end
+  end
+end
